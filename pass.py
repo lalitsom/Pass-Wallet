2c82776839d859c63efe491fe2a834b9c6e436d2
Lalit-som <Lalitsom27@gmail.com>
2017-03-31 22:02:50 +0530

    renamed main variable and use enter key to login

--- pass.py
+++ pass.py
@@ -1,20 +1,20 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 
-main=tk.Tk()
-main.configure(background="grey74")
-main.geometry("400x200")
+main_window=tk.Tk()   #main_window the root container
+main_window.configure(background="grey74")
+main_window.geometry("400x200")
 
 
 
 
-def click_enter():
+def click_enter(self):
       global frame1,frame2,store_pass
       check_pass=store_pass.get()
       if (check_pass=="walletpassword"):
           frame1.destroy()
-          main.geometry("500x500")
-          frame2 = tk.LabelFrame (main , text = "PASS-WALLET")
+          main_window.geometry("500x500")
+          frame2 = tk.LabelFrame (main_window , text = "PASS-WALLET")
           frame2.pack(fill="x",expand=False)
           heading=tk.Message(frame2, text ="Enter your secret data", width=125)
           heading.pack(fill="x",expand=True)
@@ -26,7 +26,7 @@ def click_enter():
 
           scrollbar.config(command=textarea.yview)
 
-          Add_Button=tk.Button(main , text="Save", command ="", relief= "raised" )
+          Add_Button=tk.Button(main_window , text="Save", command ="", relief= "raised" )
           Add_Button.place(x=250,y=440)
           Enter_Button.destroy()
       else:
@@ -37,15 +37,16 @@ def click_enter():
 
 
 
-frame1 = tk.LabelFrame (main , text = "PASS-WALLET")
+frame1 = tk.LabelFrame (main_window , text = "PASS-WALLET")
 frame1.place(relx=0.5,rely=0.5, anchor="center")
 tk.Message(frame1, text ="Enter the password to access your wallet", width=125).grid(row = 0,column = 0 )
 store_pass= tk.StringVar()
 init_pass=tk.Entry(frame1,show="•",textvariable=store_pass).grid(row = 0 ,column = 1 )
+main_window.bind('<Return>', click_enter)  #whenever enterkey is pressed try opening the wallet
 
-Enter_Button=tk.Button(main , text="Enter", command = click_enter, relief= "raised" )
+Enter_Button=tk.Button(main_window , text="Enter", command = click_enter, relief= "raised" )
 Enter_Button.place(x=180,y=140)
 
 
 
-main.mainloop()
+main_window.mainloop()
